b85c0cda82902c29e6ff733ee40ddb648f57e030
kyrusho <jshn2004@hotmail.com>
2024-12-11 22:08:11 -0500

    completed frontend implementation

---
+++ NoodleStar/noodlestar-frontend/src/features/model/orderRequestModel.ts
@@ -0,0 +1,12 @@
+export interface OrderDetailsRequestModel {
+    menuId: string;
+    quantity: number;
+}
+
+export interface OrderRequestModel {
+    customerId: string;
+    status?: string;
+    orderDate?: string;
+    orderDetails: OrderDetailsRequestModel[];
+    total?: number;
+}
