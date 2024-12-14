6bb618356511d1a0967889fa2a9ceeff30f7c163
Sunveer <142905066+Sunveerg@users.noreply.github.com>
2024-12-14 16:34:39 -0500

    This testing for the backend and e2e for add reviews

---
+++ NoodleStar/noodlestar-frontend/e2e/tests/AddReview.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/home');
+  await page.getByRole('button', { name: 'Go to Reviews' }).click();
+  await page.getByRole('button', { name: 'Add' }).click();
+  await page.getByPlaceholder('Enter rating').click();
+  await page.getByPlaceholder('Enter rating').fill('5');
+  await page.getByPlaceholder('Enter your name').click();
+  await page.getByPlaceholder('Enter your name').fill('Sunveer');
+  await page.getByPlaceholder('Enter your review').click();
+  await page.getByPlaceholder('Enter your review').fill('Very Good Service!');
+  await page.getByRole('button', { name: 'Submit Review' }).click();
+  await page.goto('http://localhost:3000/review');
+  await expect(page.locator('div').filter({ hasText: /^★★★★★SunveerVery Good Service!12\/14\/2024$/ }).first()).toBeVisible();
+});
---
+++ NoodleStar/noodlestar-frontend/e2e/tests/AddReviewRatingTooHigh.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/home');
+  await page.getByRole('button', { name: 'Go to Reviews' }).click();
+  await page.getByRole('button', { name: 'Add' }).click();
+  await page.getByPlaceholder('Enter rating').click();
+  await page.getByPlaceholder('Enter rating').fill('10');
+  await page.getByPlaceholder('Enter your name').fill('Sunveer');
+  await page.getByPlaceholder('Enter your review').click();
+  await page.getByPlaceholder('Enter your review').fill('Amazing!');
+  await page.getByRole('button', { name: 'Submit Review' }).click();
+  await expect(page.getByText('Rating must be between 1 and')).toBeVisible();
+});
