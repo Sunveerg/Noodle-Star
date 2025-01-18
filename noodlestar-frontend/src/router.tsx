586e11c972c17e830565e0e639b5da0c5c2033f5
kyrusho <jshn2004@hotmail.com>
2025-01-18 17:20:22 -0500

    completed frontend and playwright tests

--- noodlestar-frontend/src/router.tsx
+++ noodlestar-frontend/src/router.tsx
@@ -13,6 +13,7 @@ import EmailSentPage from './pages/EmailSentPage';
 import CallbackPage from './pages/Callbackpage';
 import ProfilePage from './pages/ProfilePage';
 import ManageStaffPage from './pages/ManageStaffPage';
+import AddStaffPage from './pages/AddStaffPage';
 
 const router = createBrowserRouter([
   {
@@ -68,6 +69,10 @@ const router = createBrowserRouter([
   {
     path: PathRoutes.ManageStaff,
     element: <ManageStaffPage />,
+  },
+  {
+    path: PathRoutes.ManageUsers,
+    element: <AddStaffPage />,
   },
 ]);
 
